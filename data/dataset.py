4b3b47d582839c652878202f7e1752bd71820665
DESKTOP-A45213N\Asad <m.asadh@outlook.com>
2023-08-06 01:22:15 -0400

    bug fix

--- data/dataset.py
+++ data/dataset.py
@@ -523,29 +523,29 @@ class MotionDatasetV2(data.Dataset):
 
         self.cumsum = np.cumsum([0] + self.lengths)
 
-        if opt.is_train:
-            # root_rot_velocity (B, seq_len, 1)
-            std[0:1] = std[0:1] / opt.feat_bias
-            # root_linear_velocity (B, seq_len, 2)
-            std[1:3] = std[1:3] / opt.feat_bias
-            # root_y (B, seq_len, 1)
-            std[3:4] = std[3:4] / opt.feat_bias
-            # ric_data (B, seq_len, (joint_num - 1)*3)
-            std[4: 4 + (joints_num - 1) * 3] = std[4: 4 + (joints_num - 1) * 3] / 1.0
-            # rot_data (B, seq_len, (joint_num - 1)*6)
-            std[4 + (joints_num - 1) * 3: 4 + (joints_num - 1) * 9] = std[4 + (joints_num - 1) * 3: 4 + (
-                        joints_num - 1) * 9] / 1.0
-            # local_velocity (B, seq_len, joint_num*3)
-            std[4 + (joints_num - 1) * 9: 4 + (joints_num - 1) * 9 + joints_num * 3] = std[
-                                                                                       4 + (joints_num - 1) * 9: 4 + (
-                                                                                                   joints_num - 1) * 9 + joints_num * 3] / 1.0
-            # foot contact (B, seq_len, 4)
-            std[4 + (joints_num - 1) * 9 + joints_num * 3:] = std[
-                                                              4 + (joints_num - 1) * 9 + joints_num * 3:] / opt.feat_bias
+        # if opt.is_train:
+        #     # root_rot_velocity (B, seq_len, 1)
+        #     std[0:1] = std[0:1] / opt.feat_bias
+        #     # root_linear_velocity (B, seq_len, 2)
+        #     std[1:3] = std[1:3] / opt.feat_bias
+        #     # root_y (B, seq_len, 1)
+        #     std[3:4] = std[3:4] / opt.feat_bias
+        #     # ric_data (B, seq_len, (joint_num - 1)*3)
+        #     std[4: 4 + (joints_num - 1) * 3] = std[4: 4 + (joints_num - 1) * 3] / 1.0
+        #     # rot_data (B, seq_len, (joint_num - 1)*6)
+        #     std[4 + (joints_num - 1) * 3: 4 + (joints_num - 1) * 9] = std[4 + (joints_num - 1) * 3: 4 + (
+        #                 joints_num - 1) * 9] / 1.0
+        #     # local_velocity (B, seq_len, joint_num*3)
+        #     std[4 + (joints_num - 1) * 9: 4 + (joints_num - 1) * 9 + joints_num * 3] = std[
+        #                                                                                4 + (joints_num - 1) * 9: 4 + (
+        #                                                                                            joints_num - 1) * 9 + joints_num * 3] / 1.0
+        #     # foot contact (B, seq_len, 4)
+        #     std[4 + (joints_num - 1) * 9 + joints_num * 3:] = std[
+        #                                                       4 + (joints_num - 1) * 9 + joints_num * 3:] / opt.feat_bias
 
-            assert 4 + (joints_num - 1) * 9 + joints_num * 3 + 4 == mean.shape[-1]
-            np.save(pjoin(opt.meta_dir, 'mean.npy'), mean)
-            np.save(pjoin(opt.meta_dir, 'std.npy'), std)
+        #     assert 4 + (joints_num - 1) * 9 + joints_num * 3 + 4 == mean.shape[-1]
+        #     np.save(pjoin(opt.meta_dir, 'mean.npy'), mean)
+        #     np.save(pjoin(opt.meta_dir, 'std.npy'), std)
 
         self.mean = mean
         self.std = std
@@ -628,7 +628,10 @@ class RawTextDataset(data.Dataset):
         pos_one_hots = []
         word_embeddings = []
         for token in tokens:
-            word_emb, pos_oh = self.w_vectorizer[token]
+            try:
+                word_emb, pos_oh = self.w_vectorizer[token]
+            except:
+                word_emb, pos_oh = self.w_vectorizer['unk/OTHER']
             pos_one_hots.append(pos_oh[None, :])
             word_embeddings.append(word_emb[None, :])
         pos_one_hots = np.concatenate(pos_one_hots, axis=0)
